07c35e8cb8a5dc366200ff9ad6e0d4a6822942f8
Android_Lucifer <89004415+Lucifer-010@users.noreply.github.com>
2024-04-01 15:07:52 +0100

    Update models.py

--- Green/models.py
+++ Green/models.py
@@ -194,7 +194,7 @@ class Otp(models.Model):
                     message.attach(part2)
 
                     context = ssl.create_default_context()
-                    with smtplib.SMTP_SSL("smtp.gmail.com", 587, context=context) as server:
+                    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
                         server.login(sender_email, password)
                         server.sendmail(
                             sender_email, receiver_email, message.as_string()
