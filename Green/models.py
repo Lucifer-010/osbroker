de0882b0ba86707e0c2d2ae1d67fd13d70138f07
Lucifer-010 <agwumafamelody@yahoo.com>
2024-06-19 01:27:28 -0400

    modified input fields

--- Green/models.py
+++ Green/models.py
@@ -100,7 +100,7 @@ class Coin(models.Model):
     name = models.CharField(max_length=200,null=False,blank=False)
     value = models.DecimalField(max_digits=999,blank=False,null=False,decimal_places=6)
     mine_rate = models.DecimalField(max_digits=999,blank=False,null=False,decimal_places=2)
-    image = models.ImageField(storage=S3Boto3Storage())
+    image = models.FileField(storage=S3Boto3Storage())
     date = models.DateTimeField(auto_now_add=True)
     class Meta:
         ordering=("-date",)
