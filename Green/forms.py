8b26f5c05581efde282a99ccfb98cab94cd45c5a
Android_Lucifer <agwumafamelody@yahoo.com>
2024-07-02 15:06:16 +1300

    Update forms.py

--- Green/forms.py
+++ Green/forms.py
@@ -6,9 +6,8 @@ from django.contrib.auth import get_user_model
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from django.core.exceptions import ValidationError
-from .models import UserDetail,TransferFund,Report
-from django.forms import TextInput, Select
-from phonenumber_field.formfields import SplitPhoneNumberField
+from .models import  UserInfo,Deposit,Withdraw,Photo
+
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control',"placeholder":"name@example.com"}))
@@ -44,120 +43,96 @@ class PasswordForm(SetPasswordForm):
         fields = ['password1','password2']
     def __init__(self,*arg,**kwarg):
         super(PasswordForm,self).__init__(*arg,**kwarg)
-        self.fields['new_password1'].widget.attrs['class']='form-control'
-        self.fields['new_password2'].widget.attrs['class']='form-control'
+        self.fields['new_password1'].widget.attrs['class']='input-box'
+        self.fields['new_password2'].widget.attrs['class']='input-box'
         self.fields['new_password1'].widget.attrs['placeholder']='New Passworld'
         self.fields['new_password2'].widget.attrs['placeholder']='Confirm Password'
 
 #
-class StaffForm(ModelForm):
+class UserForm(ModelForm):
     class Meta:
-        model =    UserDetail
-        fields = ("firstname","lastname","othername","birth","gender",
-                  "contact","emergency_name","emergency_contact","nationality","address","license_image","pin","verify_pin",)
-        #contact = PhoneNumberField(widget=PhoneNumberPrefixWidget(widgets=[Select,TextInput],attrs=None))
-        contact = SplitPhoneNumberField()
-        emergency_contact = SplitPhoneNumberField()
+        model =    UserInfo
+        fields = ("firstname","lastname",
+                  "contact","nationality","address","currency","birth",)
     def __init__(self,*arg,**kwarg):
-        super(StaffForm,self).__init__(*arg,**kwarg)
+        super(UserForm,self).__init__(*arg,**kwarg)
+        
         self.fields["firstname"].widget.attrs['placeholder']='Firstname'
         self.fields["firstname"].widget.attrs['class']='form-control'
         self.fields["lastname"].widget.attrs['placeholder']='Lastname'
         self.fields["lastname"].widget.attrs['class']='form-control'
-        self.fields["othername"].widget.attrs['placeholder']='Othername'
-        self.fields["othername"].widget.attrs['class']='form-control'
-        self.fields['birth'].widget = forms.widgets.DateInput(attrs={"type":"date","min":"1910-01-01","placeholder":"yyyy-mm-dd (DOB)","class":"form-control butpad"})
-        self.fields["gender"].widget.attrs['placeholder']='Gender'
-        self.fields["gender"].widget.attrs['class']='form-control'
-        #self.fields["contact"].widget.attrs['class']='menu form-control'
-        #self.fields["contact"].widget.attrs['placeholder']='(555) 555-1234'
         
-        #self.fields["emergency_contact"].widget.attrs['class']='menu form-control'
-        #self.fields["emergency_contact"].widget.attrs['placeholder']='(555) 555-1234'
-        self.fields["emergency_name"].widget.attrs['placeholder']='Emergency Name'
-        self.fields["emergency_name"].widget.attrs['class']='form-control'
+        
+        
+      
+        self.fields['contact'].widget=PhoneNumberPrefixWidget(
+                                                country_attrs={'class':'select mb-2'},
+                                                initial="US",
+                                                country_choices=None,
+                                                number_attrs={'class':'menu form-control',"placeholder":"(555) 555-1234"},
+                                                )
         
         self.fields["nationality"].widget.attrs['placeholder']='Nationality'
         self.fields["nationality"].widget.attrs['class']='form-control'
         self.fields["address"].widget.attrs['placeholder']='Address'
         self.fields["address"].widget.attrs['class']='form-control'
-        #self.fields["license_image"].widget.attrs['label']='Driver License Upload'
-        self.fields["license_image"].widget.attrs['class']='form-control input input-box custom-file-input'
-        self.fields["pin"].widget = forms.widgets.PasswordInput(attrs={"type":"password","pattern":"[0-9]{4}","maxlength":4,"class":"form-control","data-bs-toggle":"password"})
-        self.fields["verify_pin"].widget = forms.widgets.PasswordInput(attrs={"type":"password","pattern":"[0-9]{4}","maxlength":4,"class":"form-control","data-bs-toggle":"password"})
+        self.fields['birth'].widget = forms.DateInput(
+            attrs={
+                'class': 'datepicker form-control',  # Add a CSS class for styling (optional)
+                'placeholder': '1900-01-31',
+                'type': 'date',# Add a placeholder text (optional)
+                # Add any other attributes you want to customize
+            }
+        )
+        self.fields["currency"].widget.attrs['placeholder']='Currency'
+        self.fields["currency"].widget.attrs['class']='form-control'
+
         
-class TransferForm(ModelForm):
+class DepositForm(ModelForm):
     class Meta:
-        model =    TransferFund
-        fields = ("account_name","bank_name","account_number","ammount","swift_code","description","pin",)
+        model =    Deposit
+        fields = ("ammount","fund","pay_in",)
     def __init__(self,*arg,**kwarg):
-        super(TransferForm,self).__init__(*arg,**kwarg)
+        super(DepositForm,self).__init__(*arg,**kwarg)
+        self.fields["ammount"].widget.attrs['placeholder']='USD 0.00'
+        self.fields["ammount"].widget.attrs['class']='form-control input input-box'
+        self.fields["fund"].widget.attrs['placeholder']='Choose Wallet To Fund'
+        self.fields["fund"].widget.attrs['class']='form-control input input-box'
+        self.fields["pay_in"].widget.attrs['placeholder']=''
+        self.fields["pay_in"].widget.attrs['class']='form-control input input-box'
+        
+class WithdrawForm(ModelForm):
+    class Meta:
+        model =   Withdraw  
+        fields = ("ammount","fromacc","wallet","address","account_name","account_number","paypal","cashapp","bank_name",)
+    def __init__(self,*arg,**kwarg):
+        super(WithdrawForm,self).__init__(*arg,**kwarg)
+        self.fields["ammount"].widget.attrs['placeholder']='USD 0.00'
+        self.fields["ammount"].widget.attrs['class']='form-control input input-box'
+        self.fields["fromacc"].widget.attrs['placeholder']=''
+        self.fields["wallet"].widget.attrs['class']='form-control input input-box'
+        self.fields["wallet"].widget.attrs['placeholder']=''
+        self.fields["fromacc"].widget.attrs['class']='form-control input input-box'
+        self.fields["address"].widget.attrs['placeholder']='Wallet Address'
+        self.fields["address"].widget.attrs['class']='form-control input input-box'
+        self.fields["account_name"].widget.attrs['class']='form-control input input-box'
         self.fields["account_name"].widget.attrs['placeholder']='Account Name'
-        self.fields["account_name"].widget.attrs['class']='form-control'
-        self.fields["bank_name"].widget.attrs['placeholder']='Select Bank'
-        self.fields["bank_name"].widget.attrs['class']='form-control'
-        self.fields["swift_code"].widget.attrs['placeholder']='Swift code/Routing number'
-        self.fields["swift_code"].widget.attrs['class']='form-control'
-        self.fields["account_number"].widget.attrs['placeholder']='Enter Account Number'
-        self.fields["account_number"].widget.attrs['class']='form-control'
-        self.fields["ammount"].widget.attrs['placeholder']='$0.0'
-        self.fields["ammount"].widget.attrs['class']='form-control'
-        self.fields["ammount"].widget.attrs['id']='myNumberInput'
-        self.fields["description"].widget.attrs['placeholder']='Payment Description'
-        self.fields["description"].widget.attrs['class']='form-control'
-        self.fields["pin"].widget = forms.widgets.PasswordInput(attrs={"type":"password","pattern":"[0-9]{4}","maxlength":4,"id":"transfer_number","class":"form-control","data-bs-toggle":"password"})
+        self.fields["account_number"].widget.attrs['class']='form-control input input-box'
+        self.fields["account_number"].widget.attrs['placeholder']='Account Number'
+        self.fields["bank_name"].widget.attrs['class']='form-control input input-box'
+        self.fields["bank_name"].widget.attrs['placeholder']='Bank Name'
+        self.fields["paypal"].widget.attrs['class']='form-control input input-box'
+        self.fields["paypal"].widget.attrs['placeholder']='email@paypal.com'
+        self.fields["cashapp"].widget.attrs['class']='form-control input input-box'
+        self.fields["cashapp"].widget.attrs['placeholder']='Cashapp tag'
 
-class ReportForm(ModelForm):
+class PhotoForm(ModelForm):
     class Meta:
-        model =    Report
-        fields = ("title","subject","ref",)
+        model =    Photo
+        fields = ("image",)
     def __init__(self,*arg,**kwarg):
-        super(ReportForm,self).__init__(*arg,**kwarg)
-        self.fields["title"].widget.attrs['placeholder']='Title'
-        self.fields["title"].widget.attrs['class']='form-control'
-        self.fields["subject"].widget.attrs['placeholder']='Subject'
-        self.fields["subject"].widget.attrs['class']='form-control'
-        self.fields["ref"].widget.attrs['placeholder']='Ref'
-        self.fields["ref"].widget.attrs['class']='form-control'
-
-
-class editStaffForm(ModelForm):
-    class Meta:
-        model =    UserDetail
-        fields = ("firstname","lastname","othername","birth","gender","email",
-                  "contact","emergency_name","emergency_contact","nationality","address","license_image","pin","verify_pin",)
-        contact = SplitPhoneNumberField()
-        emergency_contact = SplitPhoneNumberField()
-    def __init__(self,*arg,**kwarg):
-        super(editStaffForm,self).__init__(*arg,**kwarg)
+        super(PhotoForm,self).__init__(*arg,**kwarg)
+        self.fields["image"].widget.attrs['placeholder']='Profile Upload'
+        self.fields["image"].widget.attrs['class']='form-control input input-box'
         
-        self.fields["firstname"].widget.attrs['placeholder']='Firstname'
-        self.fields["firstname"].widget.attrs['class']='form-control'
-        self.fields["lastname"].widget.attrs['placeholder']='Lastname'
-        self.fields["lastname"].widget.attrs['class']='form-control'
-        self.fields["othername"].widget.attrs['placeholder']='Othername'
-        self.fields["othername"].widget.attrs['class']='form-control'
-        
-        self.fields['birth'].widget = forms.widgets.DateInput(attrs={"type":"date","min":"1910-01-01","placeholder":"yyyy-mm-dd (DOB)","class":"form-control butpad"})
-        self.fields["gender"].widget.attrs['placeholder']='Gender'
-        self.fields["gender"].widget.attrs['class']='form-control'
-        
-        self.fields["email"].widget.attrs['placeholder']='Enter email'
-        self.fields["email"].widget.attrs['class']='form-control'
-        #self.fields["contact"].widget.attrs['class']='menu form-control'
-        #self.fields["contact"].widget.attrs['placeholder']='(555) 555-1234'
-        #self.fields["emergency_contact"].widget.attrs['class']='menu form-control'
-        #self.fields["emergency_contact"].widget.attrs['placeholder']='(555) 555-1234'
-        
-        self.fields["emergency_name"].widget.attrs['placeholder']='Emergency Name'
-        self.fields["emergency_name"].widget.attrs['class']='form-control'
-        
-        self.fields["nationality"].widget.attrs['placeholder']='Nationality'
-        self.fields["nationality"].widget.attrs['class']='form-control'
-        self.fields["address"].widget.attrs['placeholder']='Address'
-        self.fields["address"].widget.attrs['class']='form-control'
-        #self.fields["license_image"].widget.attrs['label']='Driver License Upload'
-        self.fields["license_image"].widget.attrs['class']='form-control input input-box custom-file-input'
-        self.fields["pin"].widget = forms.widgets.PasswordInput(attrs={"type":"password","pattern":"[0-9]{4}","maxlength":4,"class":"form-control","data-bs-toggle":"password"})
-        self.fields["verify_pin"].widget = forms.widgets.PasswordInput(attrs={"type":"password","pattern":"[0-9]{4}","maxlength":4,"class":"form-control","data-bs-toggle":"password"})
         
